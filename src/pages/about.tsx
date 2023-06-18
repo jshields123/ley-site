a72b2e8eb7f5e8f56e58b8cc56b23e73c90551c9
jshields123 <jackshields@live.co.uk>
2023-06-18 12:52:28 +1000

    added basic pages and removed unused code

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import styles from "../app/page.module.css";
+import Layout from "../app/layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <main className={styles.main}>
+        <div>
+          <h1>About LEY</h1>
+        </div>
+      </main>
+    </Layout>
+  );
+}
