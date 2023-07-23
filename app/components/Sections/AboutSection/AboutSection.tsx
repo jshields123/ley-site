cfa634c888f8a45553eeb4f96959b099bcd11681
jshields123 <jackshields@live.co.uk>
2023-07-23 20:39:11 +1000

    refactored mobile view further

--- app/components/Sections/AboutSection/AboutSection.tsx
+++ app/components/Sections/AboutSection/AboutSection.tsx
@@ -6,13 +6,6 @@ const AboutSection = () => {
   return (
     <div className={styles.about_container}>
       <div className={styles.about_content}>
-        <div className={styles.about_text}>
-          <TextCard
-            text={
-              "Over 15 years experience working across the UK & Australia. I am a passionate relationship builder, recruiter, trainer and leader. My industry experience spans from global corporate, tech start-up to not-for-profit and healthcare; one thing remains consistent, my passion for people 💡"
-            }
-          />
-        </div>
         <div className={styles.about_image}>
           <Image
             className={styles.image}
@@ -20,6 +13,13 @@ const AboutSection = () => {
             alt={"Lauren at work"}
             width={160}
             height={160}
+          />
+        </div>
+        <div className={styles.about_text}>
+          <TextCard
+            text={
+              "Over 15 years experience working across the UK & Australia. I am a passionate relationship builder, recruiter, trainer and leader. My industry experience spans from global corporate, tech start-up to not-for-profit and healthcare; one thing remains consistent, my passion for people"
+            }
           />
         </div>
       </div>
