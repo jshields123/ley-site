6062acbe00512a4b878ef4a2adeddea87db522b2
jshields123 <jackshields@live.co.uk>
2023-08-23 18:39:16 +1000

    fix for the intro image aspect ratio

--- app/components/ImageOverlay/ImageOverlay.tsx
+++ app/components/ImageOverlay/ImageOverlay.tsx
@@ -1,18 +1,29 @@
-import styles from "./ImageOverlay.module.css";
-import { ReactNode } from "react";
-import Image from "next/image";
+import styles from './ImageOverlay.module.css';
+import { ReactNode } from 'react';
+import Image from 'next/image';
 
 interface ImageOverlayProps {
+  alt: string;
   children: string | ReactNode;
   imageSrc: string;
-  alt: string;
+  isMobile?: boolean;
+  shouldHavePriority?: boolean;
   type: string;
 }
 
-const ImageOverlay = ({ children, imageSrc, alt, type }: ImageOverlayProps) => {
+const ImageOverlay = ({ children, imageSrc, alt, type, shouldHavePriority, isMobile }: ImageOverlayProps) => {
   return (
     <div className={styles.container}>
-      <Image src={imageSrc} alt={alt} fill className={`${type}__image`} />
+      <Image
+        src={imageSrc}
+        alt={alt}
+        fill={isMobile ? undefined : true}
+        className={`${type}__image`}
+        priority={shouldHavePriority ? true : undefined}
+        style={{ objectFit: 'cover' }}
+        height={isMobile ? 1080 : undefined}
+        width={isMobile ? 1920 : undefined}
+      />
       <div className={styles.overlay}>{children}</div>
     </div>
   );
--- app/components/Sections/IntroSection/IntroSection.tsx
+++ app/components/Sections/IntroSection/IntroSection.tsx
@@ -1,41 +1,29 @@
-import styles from "../IntroSection/IntroSection.module.css";
-import { ImageOverlay } from "../../ImageOverlay/ImageOverlay";
-import { Title } from "../../Title/Title";
-import { SubTitle } from "../../SubTitle";
-import { Button } from "../../Button/Button";
-import { Navbar } from "../../Navbar/Navbar";
-import { TertiaryTitle } from "../../TertiaryTitle";
+import styles from '../IntroSection/IntroSection.module.css';
+import { ImageOverlay } from '../../ImageOverlay/ImageOverlay';
+import { Title } from '../../Title/Title';
+import { SubTitle } from '../../SubTitle';
+import { Button } from '../../Button/Button';
+import { Navbar } from '../../Navbar/Navbar';
+import { TertiaryTitle } from '../../TertiaryTitle';
 
 const IntroSection = () => {
+  const isMobile = window.innerWidth <= 768;
   const onClick = () => {
-    window.open("tel:+610431471512");
+    window.open('tel:+610431471512');
   };
   return (
     <div className={styles.intro_container}>
-      <ImageOverlay
-        imageSrc={"/images/lauren1.jpeg"}
-        alt={"lauren"}
-        type={"intro"}
-      >
+      <ImageOverlay imageSrc={'/images/lauren1.jpeg'} alt={'lauren'} type={'intro'} shouldHavePriority={true} isMobile={isMobile}>
         <div className={styles.overlay}>
           <div className={styles.nav}>
             <Navbar />
           </div>
           <div className={styles.intro_text}>
-            <Title text={"LEY Consulting"} />
-            <SubTitle text={"Empowering Professionals, Elevating Businesses"} />
-            <TertiaryTitle
-              text={
-                "Your trusted partner for recruitment services, sales consulting and HR software consulting"
-              }
-            />
+            <Title text={'LEY Consulting'} />
+            <SubTitle text={'Empowering Professionals, Elevating Businesses'} />
+            <TertiaryTitle text={'Your trusted partner for recruitment services, sales consulting and HR software consulting'} />
             <div className={styles.button}>
-              <Button
-                buttonText={"Call now"}
-                type={"button"}
-                isDisabled={false}
-                onClick={onClick}
-              />
+              <Button buttonText={'Call now'} type={'button'} isDisabled={false} onClick={onClick} />
             </div>
           </div>
         </div>
