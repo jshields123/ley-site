77059bf2a6343d0bafd777839ad963cb1b208d8b
jshields123 <jackshields@live.co.uk>
2023-08-24 10:51:41 +1000

    adding in title and meta tags for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { Inter } from 'next/font/google';
 import styles from './layout.module.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +12,14 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <html lang="en" className={styles.html}>
-      <title>LEY Consulting</title>
+      <Head>
+        <meta
+          name="description"
+          content="Empowering Professionals, Elevating Businesses | Your trusted partner for recruitment services, sales consulting and HR software consulting"
+          key={'description'}
+        />
+        <title>LEY Consulting - Recruitment, Sales and HR Specialist</title>
+      </Head>
       <body className={styles.layout}>{children}</body>
     </html>
   );
