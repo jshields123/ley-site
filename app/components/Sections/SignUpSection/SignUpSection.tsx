253be7e7497471db76f25dc0e88c5ace00e7d3a9
jshields123 <jackshields@live.co.uk>
2023-08-27 12:46:31 +1000

    added in responsive removal of image overlay for mob

--- app/components/Sections/SignUpSection/SignUpSection.tsx
+++ app/components/Sections/SignUpSection/SignUpSection.tsx
@@ -1,15 +1,27 @@
 import styles from './SignUpSection.module.css';
 import { FormCard } from '../../Cards/FormCard/FormCard';
 import { ImageOverlay } from '../../ImageOverlay';
+import { useState, useEffect } from 'react';
 
 const SignUpSection = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const newIsMobile = window.innerWidth <= 768;
+    setIsMobile(newIsMobile);
+  }, []);
+
   return (
     <div className={styles.signup_container}>
-      <ImageOverlay imageSrc={'/images/business-meeting.jpeg'} alt={'business meeting'} type={'form'}>
-        <div className={styles.overlay}>
-          <FormCard />
-        </div>
-      </ImageOverlay>
+      {isMobile ? (
+        <FormCard />
+      ) : (
+        <ImageOverlay imageSrc={'/images/business-meeting.jpeg'} alt={'business meeting'} type={'form'}>
+          <div className={styles.overlay}>
+            <FormCard />
+          </div>
+        </ImageOverlay>
+      )}
     </div>
   );
 };
