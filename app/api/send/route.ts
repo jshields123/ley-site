ff2d824a2f9d45efe8dd09c927bf9686412573e5
jshields123 <jackshields@live.co.uk>
2023-09-23 14:43:49 +1000

    added in next public prefix to api keys

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from '../../components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(req: any) {
   const { firstName, lastName, company, phone, email } = await req.json();
